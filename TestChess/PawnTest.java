f370aa0ece66a1171c3e2c2adc6fe245d9af32ca
TaekSoon <taeksoon.jang@gmail.com>
2013-07-12 15:42:27 +0900

    1-6 finish by.taeksoon, moonchul

--- TestChess/PawnTest.java
+++ TestChess/PawnTest.java
@@ -1,15 +1,18 @@
 import junit.framework.TestCase;
 
-
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
-		Pawn pawn = new Pawn("white");
-		
+
+		String white = "white";
+		String black = "black";
+
+		Pawn pawn = new Pawn(white);
+
 		String color = pawn.getColor();
-		assertEquals("white", color);
-		
-		Pawn pawn2 = new Pawn("black");
+		assertEquals(white, color);
+
+		Pawn pawn2 = new Pawn(black);
 		color = pawn2.getColor();
-		assertEquals("black", color);
+		assertEquals(black, color);
 	}
 }
