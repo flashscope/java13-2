9507266479890710d31b2b63f47dd64600163f78
flashscope <flashscope@naver.com>
2013-07-18 01:10:27 +0900

    3-1 add Character class

---
+++ TestChess/CharacterTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+
+public class CharacterTest extends TestCase {
+	public void testWhitespace() throws Exception {
+		assertEquals(true, Character.isWhitespace('\n'));
+		assertEquals(true, Character.isWhitespace('	'));
+		assertEquals(true, Character.isWhitespace(' '));
+	}
+}
