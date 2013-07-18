3228ad1c89b1354e7edf0af70c0220a72aacbb0c
flashscope <flashscope@naver.com>
2013-07-18 22:34:03 +0900

    3-8 commit for big refactoring -- cancel

--- TestChess/pieces/PawnTest.java
+++ TestChess/pieces/PawnTest.java
@@ -3,13 +3,7 @@ import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
-
-		/*
-		 * no more need!
-		Pawn pawnWhite = new Pawn(Pawn.PAWN_WHITE,'p');
-		String color = pawnWhite.getColor();
-		assertEquals(Pawn.PAWN_WHITE, color);
-		*/
+		//nothing now
 		
 	}
 }
