ddd7523c004b3e9014625ea91202956260d73e44
flashscope <flashscope@naver.com>
2013-07-18 20:11:35 +0900

    3-1 \t fix

--- TestChess/CharacterTest.java
+++ TestChess/CharacterTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 public class CharacterTest extends TestCase {
 	public void testWhitespace() throws Exception {
 		assertEquals(true, Character.isWhitespace('\n'));
-		assertEquals(true, Character.isWhitespace('	'));
+		assertEquals(true, Character.isWhitespace('\t'));
 		assertEquals(true, Character.isWhitespace(' '));
 	}
 }
