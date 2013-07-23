784d55ebcb7d18504b8cad66bab11a26ddb8e817
flashscope <flashscope@naver.com>
2013-07-23 22:15:04 +0900

    hw4-4 add 32 pieces

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -1,56 +1,65 @@
 package chess;
+
 import java.util.ArrayList;
 
 import pieces.Piece;
 import util.StringUtil;
+
 /**
  * 체스판으로 체스말을 올려놓는 용도입니다.
+ * 
  * @author flashscope
- *
+ * 
  */
 class Board {
 	private static ArrayList<Row> boardList = new ArrayList<Row>();
-	
-	public void initialize(){
+
+	public void initialize() {
 		boardList.clear();
-		for(int i=0; i<8; ++i){
-			if(i==1){
-				boardList.add(new Row(Piece.pieceColor.PIECE_WHITE,Piece.pieceType.PAWN));
-			}else if(i==6){
-				boardList.add(new Row(Piece.pieceColor.PIECE_BLACK,Piece.pieceType.PAWN));
-			}else{
-				boardList.add(new Row(Piece.pieceColor.PIECE_NONE,Piece.pieceType.NONE));
+		for (int i = 0; i < 8; ++i) {
+			Row row = new Row();
+			switch (i) {
+			case 0:
+				row.setOtherLine(Piece.pieceColor.PIECE_BLACK);
+				break;
+			case 1:
+				row.setPawnLine(Piece.pieceColor.PIECE_BLACK);
+				break;
+			case 6:
+				row.setPawnLine(Piece.pieceColor.PIECE_WHITE);
+				break;
+			case 7:
+				row.setOtherLine(Piece.pieceColor.PIECE_WHITE);
+				break;
+			default:
+				row.setBlankLine();
+				break;
 			}
+
+			boardList.add(row);
 		}
 	}
-	
 
-	
-	public String getRowStringById(int index){
+	public String getRowStringById(int index) {
 		return boardList.get(index).getRowString();
 	}
-	
-	
-	public int getPieceSize(){
+
+	public int getPieceSize() {
 		int pieceSize = 0;
-		for(int i=0; i<8; ++i){
+		for (int i = 0; i < 8; ++i) {
 			pieceSize += boardList.get(i).getRowPieceSize();
 		}
 		return pieceSize;
 	}
-	
-	
-	public StringBuilder printBoard(){
+
+	public String printBoard() {
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<8; ++i){
+		for (int i = 0; i < 8; ++i) {
 			sb.append(StringUtil.appendNewLine(boardList.get(i).getRowString()));
 		}
 		System.out.println(sb);
-		
-		return sb;
-	}
-	
 
-	
+		return sb.toString();
+	}
 
 }
--- src/chess/Row.java
+++ src/chess/Row.java
@@ -6,17 +6,33 @@ import pieces.Piece;
 
 class Row {
 	ArrayList<Piece> rowList = new ArrayList<Piece>();
-
-	Row(Piece.pieceColor color, Piece.pieceType type) {
+	
+	public void setPawnLine(Piece.pieceColor color) {
 		rowList.clear();
-		
 		for (int i = 0; i < 8; ++i) {
-			rowList.add(new Piece(color,type));
+			rowList.add(new Piece(color, Piece.pieceType.PAWN));
 		}
-		
 	}
-
-
+	
+	public void setBlankLine() {
+		rowList.clear();
+		for (int i = 0; i < 8; ++i) {
+			rowList.add(new Piece(Piece.pieceColor.PIECE_NONE, Piece.pieceType.NONE));
+		}
+	}
+	
+	public void setOtherLine(Piece.pieceColor color) {
+		rowList.clear();
+		rowList.add(new Piece(color, Piece.pieceType.ROOK));
+		rowList.add(new Piece(color, Piece.pieceType.KNIGHT));
+		rowList.add(new Piece(color, Piece.pieceType.BISHOP));
+		rowList.add(new Piece(color, Piece.pieceType.QUEEN));
+		rowList.add(new Piece(color, Piece.pieceType.KING));
+		rowList.add(new Piece(color, Piece.pieceType.BISHOP));
+		rowList.add(new Piece(color, Piece.pieceType.KNIGHT));
+		rowList.add(new Piece(color, Piece.pieceType.ROOK));
+	}
+	
 	public String getRowString() {
 		StringBuilder sb = new StringBuilder();
 		for (int j = 0; j < 8; ++j) {
@@ -25,17 +41,15 @@ class Row {
 
 		return sb.toString();
 	}
-	
-	
-	public int getRowPieceSize(){
+
+	public int getRowPieceSize() {
 		int pieceSize = 0;
-		for(int i=0; i<8; ++i){
-			if(rowList.get(i).getColor()!=Piece.pieceColor.PIECE_NONE){
+		for (int i = 0; i < 8; ++i) {
+			if (rowList.get(i).getColor() != Piece.pieceColor.PIECE_NONE) {
 				++pieceSize;
 			}
 		}
 		return pieceSize;
 	}
-	
 
 }
