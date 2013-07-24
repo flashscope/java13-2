091d3f23460c17dc28d9c3479723f44cd0e90367
flashscope <flashscope@naver.com>
2013-07-24 18:06:38 +0900

    hw5-3 testunit writing...

--- TestChess/pieces/PawnTest.java
+++ TestChess/pieces/PawnTest.java
@@ -3,7 +3,13 @@ import junit.framework.TestCase;
 
 public class PawnTest extends TestCase {
 	public void testCreate() throws Exception {
-		//nothing now
+		verifyCreation(
+				Piece.createWhitePawn(),Piece.createBlackPawn(),
+				Piece.pieceType.PAWN, Piece.PAWN_REPRESENTATION);
+	}
+	
+	private void verifyCreation(Piece whitePiece, Piece blackPiece,
+			Piece.pieceType type, char representation){
 		
 	}
 }
